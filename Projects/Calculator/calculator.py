2412ec65a52c8ec96bee4f754e4761ba01fbaff3
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-10 21:00:07 +0100

    Day 10 Project : Calculator

--- Projects/Calculator/calculator.py
+++ Projects/Calculator/calculator.py
@@ -1,27 +1,37 @@
 from art import print_art
+import os
 
 print_art()
+first_number=float(input("What's the first number?: "))
 while(True):
-    first_number=float(input("What's the first number?: "))
+    
     print("""
-    +
-    -
-    *
-    /
++
+-
+*
+/
           """)
     operation=input("Pick an operation: ")
+    while(operation not in "+-/*"):
+            operation=input("Pick an operation: ")
     second_number=float(input("What's the next number?: "))
     
-    if(operation=="+"):
-        print(first_number+second_number)
-    elif(operation=="-"):
-        print(first_number-second_number)
-    elif(operation=="*"):
-        print(first_number*second_number)
-    elif(operation=="/"):
-        print(first_number/second_number)
-    else:
-        while(operation not in "+-/*"):
-            operation=input("Pick an operation: ")
     
-    choice=input(f"Type 'y' to contionue calculating with {}, or type 'n' to start a new calculation: ")
+    result=0
+    if(operation=="+"):
+        print(result:=first_number+second_number)
+    elif(operation=="-"):
+        print(result:=first_number-second_number)
+    elif(operation=="*"):
+        print(result:=first_number*second_number)
+    elif(operation=="/"):
+        print(result:=first_number/second_number)
+        
+    
+    choice=input(f"Type 'y' to continue calculating with {result}, or type 'n' to start a new calculation: ")
+    if(choice.strip().lower()=="y"):
+        first_number=result
+    else:
+        os.system('cls')
+        print_art()
+        first_number=float(input("What's the first number?: "))
