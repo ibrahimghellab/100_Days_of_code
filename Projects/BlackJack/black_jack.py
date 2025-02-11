27092ffabd7a1bc0d979b487b080b837ac36e894
ibrahimghellab <ghellabibrahim@gmail.com>
2025-02-11 11:36:33 +0100

    Day 11 Project : BlackJack

--- Projects/BlackJack/black_jack.py
+++ Projects/BlackJack/black_jack.py
@@ -3,57 +3,78 @@ import random
 import os
 
 
-cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+def calculate_score(player_score,bot_score):
+    if(player_score==21):
+        print("Win with a Blackjack 😎")
+    elif(bot_score==21):
+        print("Lose, opponent as Blackjack 🫠")
+    elif(player_score>=22):
+        print("You went over. You lose 😒")
+    elif(bot_score>=22):
+        print("Opponent went over. You win 😁")
+    elif(player_score<22 and bot_score<22 and player_score>bot_score):
+        print("You Win 🙌")
+    elif(player_score<22 and bot_score<22 and player_score<bot_score):
+        print("You lose 😭")
+    elif(player_score<22 and bot_score<22 and player_score==bot_score):
+        print("What a draw 💀")
 
-if(input("Do you want to play a game of BlackJack? Type 'y' or 'n':")=="y"):
-    print_art()
-    your_sum=0
-    bot_sum=0
-    player=[cards[random.randint(0,12)],cards[random.randint(0,12)]]
-    your_sum+=sum(player)
-    bot=[cards[random.randint(0,12)],cards[random.randint(0,12)]]
-    bot_sum+=sum(bot)
+
+
+def check_as(l):
+    l_sum=sum(l)
+
+    while(11 in l and l_sum>=22):
+        l[l.index(11)]=1
+
+    l_sum=sum(l)
+
+    return l_sum
     
-    print(f"Your cards : {player}, current score : {your_sum}")
-    print(f"Computer's first card: {bot_sum}")
-    if(your_sum==21):
-        print("something")
-    elif(bot_sum==21):
-        print("something else")
-    else:
-        choice=input("Type 'y' to get another card, type 'n' to pass: ")
-        while(choice.strip().lower()=='y' and your_sum <22 ):
-            
-            new_random=random.randint(0,12)
-            player.append(cards[new_random])
-            your_sum+=cards[new_random]
-            print(f"Your cards : {player}, current score : {your_sum}")
-            print(f"Computer's first card: {bot_sum}")
-            if(your_sum<22):
-                choice=input("Type 'y' to get another card, type 'n' to pass: ")
 
-        while(bot_sum<17):
-            new_random=random.randint(0,12)
-            bot.append(cards[new_random])
-            bot_sum+=cards[new_random]
+
+cards=[11,2,3,4,5,6,7,8,9,10,10,10,10]
+while(True):
+    if(input("Do you want to play a game of BlackJack? Type 'y' or 'n': ")=="y"):
+        os.system('cls')
+        print_art()
         
-        print(f"Your final hand: {player}, final score: {your_sum}")
-        print(f"Computer's final hand: {bot}, final score: {bot_sum}")
-
-
+        player=[cards[random.randint(0,12)],cards[random.randint(0,12)]]
+        your_sum=check_as(player)
+        
+        bot=[cards[random.randint(0,12)],cards[random.randint(0,12)]]
+        bot_sum=check_as(bot)
+        
+        print(f"Your cards : {player}, current score : {your_sum}")
+        print(f"Computer's first card: {bot[0]}")
         if(your_sum==21):
             print("Win with a Blackjack 😎")
         elif(bot_sum==21):
-            print("Loose with an opposent Blackjack 🫠")
-        elif(your_sum>=22):
-            print("You went over. You lose 😒")
-        elif(bot_sum>=22):
-            print("Opponent went over. You win 😁")
-        elif(your_sum<22 and bot_sum<22 and your_sum>bot_sum):
-            print("You Win 🙌")
-        elif(your_sum<22 and bot_sum<22 and your_sum<bot_sum):
-            print("You loose 😭")
+            print("Lose, opponent as Blackjack 🫠")
+        else:
+            choice=input("Type 'y' to get another card, type 'n' to pass: ")
+            while(choice.strip().lower()=='y' and your_sum <22 ):
+                
+                new_random=random.randint(0,12)
+                player.append(cards[new_random])
+                your_sum=check_as(player)
+                
+                if(your_sum<22):
+                    print(f"Your cards : {player}, current score : {your_sum}")
+                    print(f"Computer's first card: {bot[0]}")
+                    choice=input("Type 'y' to get another card, type 'n' to pass: ")
+                    
+                    
+            if(your_sum<22):
+                while(bot_sum<17):
+                    new_random=random.randint(0,12)
+                    bot.append(cards[new_random])
+                    bot_sum=check_as(bot)
+            
+            print(f"Your final hand: {player}, final score: {your_sum}")
+            print(f"Computer's final hand: {bot}, final score: {bot_sum}")
 
-
-
-#ordonne le code met les conditions au bon moment avec des if else 
+            calculate_score(player_score=your_sum,bot_score=bot_sum)
+           
+    else:
+        break
